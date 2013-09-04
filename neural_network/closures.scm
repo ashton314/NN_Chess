0c17c7535043ebfe9cead008f9b677af6959d861
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2013-09-03 21:08:52 -0600

    - filled out a bit of the neural_network/ directory

---
+++ neural_network/closures.scm
@@ -0,0 +1,4 @@
+;;; Neural Network library, using closures
+;;; Ashton Wiersdorf
+;;; Part of the NN_Chess project
+
---
+++ neural_network/compiler.scm
@@ -0,0 +1,4 @@
+;;; Neural Network library, compiling version
+;;; Ashton Wiersdorf
+;;; Part of the NN_Chess project
+
