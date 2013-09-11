78b71f91c77217754f5794e22162c685d54968e8
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2013-09-10 19:26:59 -0600

    - made the neural network test a bit more configurable

--- neural_network/t/closure_node_test.scm
+++ neural_network/t/closure_node_test.scm
@@ -4,10 +4,15 @@
 (load "../../lib/tap.scm")
 (load "../closures.scm")
 
-(plan-tests 11)
+;; (plan-tests 11)
 
 (define *input0-input* 1)
-(define *input1-input* .5)
+(define *input1-input* 1)
+
+(define *s0-weight* 1)
+(define *s1-weight* 1)
+(define *0o-weight* .5)
+(define *1o-weight* .5)
 
 (define input0 (make-node))
 
@@ -16,9 +21,9 @@
 (is ((input0 'bias) 'get) *bias* "bias set to default for input0")
 
 ((input0 'input-nodes) 'set! (list (lambda (op) *input0-input*)))
-((input0 'weights) 'set! '(.5))
+((input0 'weights) 'set! (list *s0-weight*))
 
-(is ((input0 'weights) 'get) '(.5) "weights set correctlly for input0")
+(is ((input0 'weights) 'get) (list *s0-weight*) "weights set correctlly for input0")
 
 (define input1 (make-node))
 
@@ -27,19 +32,21 @@
 (is ((input1 'bias) 'get) *bias* "bias set to default for input1")
 
 ((input1 'input-nodes) 'set! (list (lambda (op) *input1-input*)))
-((input1 'weights) 'set! '(.5))
+((input1 'weights) 'set! (list *s1-weight*))
 
-(define output (make-node `(input-nodes (,input0 ,input1)) '(weights (.5 .5))))
+(define output (make-node `(input-nodes (,input0 ,input1)) `(weights (,*0o-weight* ,*1o-weight*))))
 (define result #f)
 ((output 'output) 'set! (lambda (val) (set! result val) 'from-result))
 
 (is ((output 'input-nodes) 'get) (list input0 input1) "input nodes correct for output")
-(is ((output 'weights) 'get) (list .5 .5) "weights correct for output node")
+(is ((output 'weights) 'get) (list *0o-weight* *1o-weight*) "weights correct for output node")
 
 (is-eq (output 'run) 'from-result "output.output returned correctly")
 (ok result "result is set")
 
-(done-testing)
-
 (write-string "Result: ")
 (pretty-print result)
+(write-string "\n")
+
+
+(done-testing)
