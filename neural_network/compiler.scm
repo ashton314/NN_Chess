d567538ec3da447d9a565d082d0486bbe7a7c354
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2013-09-14 19:17:54 -0600

    - working on closure-based network compiler

--- neural_network/compiler.scm
+++ neural_network/compiler.scm
@@ -21,27 +21,41 @@
   ;; Sigmoid function
   (/ 1 (+ 1 (exp (- value)))))
 
-(define-net test
-  ;; Input nodes
-  (input-node1 (shift 1))		; `shift' ala Perl
-  (input-node2 (shift 1))
+(define-net test-net
+  (input1 input2)
+  (output1 output2)
 
   ;; Hidden layers
-  (node-a (input-node1 0.1) (input-node2 0.8))
-  (node-b (input-node1 0.4) (input-node2 0.6))
+  (node-a (input1 0.1) (input2 0.8))
+  (node-b (input1 0.4) (input2 0.6))
 
   ;; Output nodes
-  (output
-   (out1 (node-a 0.3) (node-b 0.9))
-   (out2 (node-a 0.1) (node-b 0.5))))
+  (out1 (node-a 0.3) (node-b 0.9))
+  (out2 (node-a 0.1) (node-b 0.5)))
 
+;; Root learning call
+(test-net 'train target1 target2 ...)
+
+(define-syntax create-nodes
+  (syntax-rules ()
+    ((create-nodes ((node-name (input weight) ...) ...) logic)
+     (let ((node-name '((inputs input ...)
+			(weights weight ...)
+			(value #f))) ...)
+       logic))))
 
 (define-syntax define-net
   (syntax-rules ()
-    ((define-net net-name node-descriptions ...)
-     (define (net-name . inputs)
-       (define (shift)
-	 (pop! inputs))
-       (compile-nodes node-descriptions ...)))))
+    ((define-net net-name (input-nodes ...) (output-nodes ...) node-descriptors ...)
+     (define (net-name op . inputs)
+       (create-nodes (node-descriptors ...)
+	  (case op
+	    ((run)			; inputs are in values
+	     (let ((input-nodes (pop! inputs)) ...)
+	       (map (lambda (node-name)
+		      
+		      ) output-nodes ...)
 
+	    ((train)			; two lists: first are inputs, second are target values
 
+	     )
