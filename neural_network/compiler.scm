48317f549f297c31de192d2143e8299af763ba1e
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2013-09-14 20:28:32 -0600

    - neural net works correctly (so far) with macro version - moving on to back prop algorithm

--- neural_network/compiler.scm
+++ neural_network/compiler.scm
@@ -4,6 +4,8 @@
 
 ;; Macros
 
+(load-option 'format)
+
 (define-syntax push!
   (syntax-rules ()
     ((push! datum place)
@@ -21,9 +23,58 @@
   ;; Sigmoid function
   (/ 1 (+ 1 (exp (- value)))))
 
+;; TODO: Write a sort of backwards pass through the network to set all
+;; of the nodes "outputs" values
+
+(define-syntax define-net
+  (syntax-rules ()
+    ((define-net net-name (input-nodes ...) (output-nodes ...) (node-name (input weight) ...) ...)
+     (define (net-name op . input-values)
+       (let ((nodes `(
+		      (input-nodes (inputs '())
+				   (outputs '())
+				   (weights '())
+				   (value #f)) ...
+		      (node-name (inputs (input ...))
+ 				 (outputs '())
+ 				 (value #f)
+				 (weights (weight ...))) ...
+				 )))
+
+	 (for-each (lambda (node)	; Initilize outputs
+		     (set-cdr! (assoc 'outputs (cdr node))
+			       (map car (filter (lambda (other-node)
+						  (memq (car node) (cadr (assoc 'inputs (cdr other-node))))) nodes))))
+		   nodes)
+
+	 (define (get-value node)
+	   (let ((this-node (cdr (or (assoc node nodes) (error (format #f "could not find node ~A" node))))))
+	     (let ((this-value (cadr (assoc 'value this-node))))
+	       (if this-value this-value
+		   (let ((new-value (activation (apply + (map * (map get-value (cadr (assoc 'inputs this-node)))
+							      (cadr (assoc 'weights this-node)))))))
+		     (set-car! (cdr (assoc 'value this-node)) new-value)
+		     new-value)))))
+
+	 (case op
+	   ((run)			; inputs are in values
+	    (map (lambda (node) (set-car! (cdr (assoc 'value (cdr (assoc node nodes)))) (pop! input-values))) '(input-nodes ...))
+	    (map get-value '(output-nodes ...)))
+
+	   ((debug-nodes)
+	    ;; For debugging purposes
+	    nodes)
+	      
+	   ((train)			; two lists: first are inputs, second are target values
+	    (error "to be implemented"))
+	   (else
+	    (error "bad operation on neural net"))))))))
+
+
+
 (define-net test-net
   (input1 input2)
-  (output1 output2)
+  (out1)
 
   ;; Hidden layers
   (node-a (input1 0.1) (input2 0.8))
@@ -34,40 +85,5 @@
   (out2 (node-a 0.1) (node-b 0.5)))
 
 ;; Root learning call
-(test-net 'train target1 target2 ...)
+;(test-net 'train target1 target2 ...)
 
-;; TODO: Write a sort of backwards pass through the network to set all
-;; of the nodes "outputs" values
-
-(define-syntax define-net
-  (syntax-rules ()
-    ((define-net net-name (input-nodes ...) (output-nodes ...) (node-name (input weight) ...) ...)
-     (define (net-name op . inputs)
-       (let ((nodes '((input-nodes (inputs '())
-				   (outputs '())
-				   (weights '())
-				   (value (pop! inputs))) ...
-		      (node-name (inputs input)
-				 (outputs '())
-				 (weights weights)
-				 (value #f)) ...)))
-	 (case op
-	   ((run)			; inputs are in values
-	    (let ((input-nodes (pop! inputs)) ...)
-
-	      (define (get-value node)
-		(let ((this-node (cdr (assoc node nodes))))
-		  (let ((this-value (cadr (assoc 'value this-node))))
-		    (if this-value this-value
-			(let ((new-value (apply + (map * (map get-value (cadr (assoc 'inputs this-node)))
-						       (cadr (assoc 'weights this-node))))))
-			  (set-cdr! (assoc 'value this-node) new-value)
-			  new-value)))))
-
-	      (map (lambda (node-name)
-		     
-		     ) '(output-nodes ...))
-	      
-	      ((train)			; two lists: first are inputs, second are target values
-	       
-	       )
