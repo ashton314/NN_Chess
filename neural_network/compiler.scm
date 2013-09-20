c064ab868d39c662d428149670ee5e292ea32b78
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2013-09-19 21:02:44 -0600

    - working on converting compiler.scm to /actually/ compile into   closures

--- neural_network/compiler.scm
+++ neural_network/compiler.scm
@@ -33,7 +33,8 @@
 				   (weights ())
 				   (error-delta 'undef)
 				   (value #f)) ...
-		      (node-name (inputs (input ...))
+		      (node-name (sym-name node-name)
+				 (inputs (input ...))
  				 (outputs ())
  				 (value #f)
 				 (error-delta #f)
@@ -45,55 +46,77 @@
 						  (memq (car node) (cadr (assoc 'inputs (cdr other-node))))) nodes))))
 		   nodes)
 
-	 (define (get-value node)
-	   (let ((this-node (cdr (or (assoc node nodes) (error (format #f "could not find node ~A" node))))))
-	     (let ((this-value (cadr (assoc 'value this-node))))
-	       (if this-value this-value
-		   (let ((new-value (activation (apply + (map * (map get-value (cadr (assoc 'inputs this-node)))
-							      (cadr (assoc 'weights this-node)))))))
-		     (set-car! (cdr (assoc 'value this-node)) new-value)
-		     new-value)))))
+	 (define-syntax getter-setter
+	   (syntax-rules ()
+	     ((_ loc hash)
+	      (let ((data-slot (cadr assoc loc hash)))
+		(lambda (op . rest)
+		  (if (eq? op 'get)
+		      data-slot
+		      (if (eq? op 'set!)
+			  (set! data-slot (car rest))
+			  (error (format #f "Unknown option passed to node: ~A -- ~A" op 'loc)))))))))
 
-	 (define (back-prop this-node node)
-;; 	   (format #t "##### BACKPROP: this-node: '~A' node: '~A'~%" this-node node)
-	   ;; `node' is the cdr of (assoc ,node-name nodes)
+	 (for-each (lambda (node)	; compile pass
+		     (let ((values (cdr node)))
+		       (set-car! (cdr node)
+				 (lambda (op)
+				   (case op
+				     ((sym-name) (getter-setter 'sym-name values))
+				     ((inputs) (getter-setter 'inputs values))
+				     ((outputs) (getter-setter 'outputs values))
+				     ((weights) (getter-setter 'weights values))
+				     ((error-delta) (getter-setter 'error-delta values))
+				     ((value) (getter-setter 'value values))		
+				     (else (error (format #f "Unknown option to node: ~A" op))))))))
+		   nodes)
 
-;	   (debug)
+	 (for-each (lambda (node)	; linker pass
+		     ((node 'inputs) 'set! (map (lambda (node) (cadr (assoc node nodes))) ((node 'inputs) 'get))))
+		     ((node 'outputs) 'set! (map (lambda (node) (cadr (assoc node nodes))) ((node 'outputs) 'get))))
+		   nodes)
 
-	   (define (searcher nodes weights)
-	     (if (there-exists? (list nodes weights) null?)
+	 (define (back-prop node)
+	   ;; node => closure-object
+
+	   (define (searcher search-nodes search-weights)
+	     ;; This takes a list of nodes and a list of weights,
+	     ;; which are mapped to eachother linearly. This finds the
+	     ;; weight associated with `node' (see above scope)
+
+	     ;; OPTIMIZATION NOTE: Node-weight lookup could be
+	     ;; improved through some complex mechanism in the linking
+	     ;; pass
+	     (if (there-exists? (list search-nodes search-weights) null?)
 		 (begin
 		   (debug)
-		   (error (format #f "could not find node ~A in inputs of an output node!" this-node)))
-		 (if (eq? (car nodes) this-node)
-		     (car weights)
-		     (searcher (cdr nodes) (cdr weights)))))
+		   (error (format #f "could not find node ~A in inputs of an output node!" node)))
+		 (if (eq? (car search-nodes) node)
+		     (car search-weights)
+		     (searcher (cdr search-nodes) (cdr search-weights)))))
 		 
 
-	   (let ((bad-node (find-matching-item (map (lambda (sym) (assoc sym nodes)) (cadr (assoc 'outputs node)))
+	   (let ((bad-node (find-matching-item ((node 'outputs) 'get)
 					       (lambda (out-node)
-						 (not (cadr (assoc 'error-delta (cdr out-node))))))))
+						 (not ((out-node 'error-delta) 'get))))))
 	     (if bad-node ; one of this node's output nodes' error-delta has not been computed
-		 (back-prop (car bad-node) (cdr bad-node))
+		 (back-prop bad-node)
 		 (begin
-		   (if (or (cadr (assoc 'error-delta node))
-			   (null? (assoc 'inputs node)))
+		   (if (or ((node 'error-delta) 'get)
+			   (null? ((node 'inputs) 'get)))
 		       #t			; already computed or not needed to correct
-		       (let ((delta (let ((val (cadr (assoc 'value node))))
+		       (let ((delta (let ((val ((node 'value) 'get)))
 				      (* val (- 1 val)
 					 (apply + (map (lambda (output-node)
-							 (* (cadr (assoc 'error-delta output-node))
-							    (searcher (cadr (assoc 'inputs output-node))
-								      (cadr (assoc 'weights output-node)))))
-						       (map (lambda (sym) (cdr (assoc sym nodes))) (cadr (assoc 'outputs node)))))))))
-			 (set-car! (cdr (assoc 'error-delta node)) delta)
-			 (set-car! (cdr (assoc 'weights node))
-				   (map (lambda (this-weight in-node)
-					  (+ this-weight (* learning-rate delta (cadr (assoc 'value in-node)))))
-					(let ((lambda-val (cadr (assoc 'weights node))))
-;					  (debug)
-					  lambda-val)
-					(map (lambda (sym) (cdr (assoc sym nodes))) (cadr (assoc 'inputs node)))))))
+							 (* ((output-node 'error-delta) 'get)
+							    (searcher ((output-node 'inputs) 'get)
+								      ((output-node 'weights) 'get))))
+						       ((node 'outputs) 'get)))))))
+			 ((node 'error-delta) 'set! delta)
+			 ((node 'weights) 'set! (map (lambda (this-weight in-node)
+						       (+ this-weight (* learning-rate delta ((in-node 'value) 'get))))
+						     ((node 'weights) 'get)
+						     ((node 'inputs) 'get)))))
 						 
 		   (for-each (lambda (input-node) (back-prop input-node (cdr (assoc input-node nodes)))) ; recurse to input nodes
 			     (cadr (assoc 'inputs node)))))))
@@ -115,8 +138,6 @@
 				  (map (lambda (nul) (random-real)) (cadr (assoc 'inputs node)))))
 		      (map cdr nodes)))
 
-;	    (map (lambda (o) (pretty-print o) (write-string "\n")) nodes))
-
 	   ((last-errors)
 	    (map (lambda (node) (cadr (assoc 'error-delta node))) (map (lambda (sym) (cdr (assoc sym nodes))) '(output-nodes ...))))
 
@@ -126,7 +147,6 @@
 		   (targets (cadr input-values))
 		   (first-pass (apply net-name `(run ,@inputs)))
 		   (deltas (map (lambda (output target) (* (- target output) (- 1 output) output)) first-pass targets)))
-;	      (format #t "Inputs:  ~A~%Targets: ~A~%" inputs targets)
 	      (for-each (lambda (node delta)
 			  (let ((self-node (cdr (assoc node nodes))))
 			    (set-car! (cdr (assoc 'error-delta self-node)) delta)
