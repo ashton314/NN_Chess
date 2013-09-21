8219c37b46148dbbd53c36ba3034a276c5c9af9c
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2013-09-21 17:30:16 -0600

    - nodes are now persistant between network calls - tests 1-4 pass

--- neural_network/compiler.scm
+++ neural_network/compiler.scm
@@ -26,7 +26,8 @@
 (define-syntax define-net
   (syntax-rules ()
     ((define-net net-name (input-nodes ...) (output-nodes ...) _learning-rate (node-name (input weight) ...) ...)
-     (define (net-name op . input-values)
+     (begin
+       (define net-name)
        (let ((learning-rate _learning-rate) ; prevent multiple evaluation (does this happen with Scheme's macros?)
 	     (nodes `((input-nodes (sym-name input-nodes)
 				   (inputs ())
@@ -34,19 +35,19 @@
 				   (weights ())
 				   (error-delta undef)
 				   (value #f)) ...
-		      (node-name (sym-name node-name)
-				 (inputs (input ...))
- 				 (outputs ())
- 				 (value #f)
-				 (error-delta #f)
-				 (weights (weight ...))) ...)))
-
+				   (node-name (sym-name node-name)
+					      (inputs (input ...))
+					      (outputs ())
+					      (value #f)
+					      (error-delta #f)
+					      (weights (weight ...))) ...)))
+	 
 	 (for-each (lambda (node)	; initilize outputs
 		     (set-car! (cdr (assoc 'outputs (cdr node)))
 			       (map car (filter (lambda (other-node)
 						  (memq (car node) (cadr (assoc 'inputs (cdr other-node))))) nodes))))
 		   nodes)
-
+	 
 	 (define-syntax getter-setter
 	   (syntax-rules ()
 	     ((_ place)
@@ -57,7 +58,7 @@
 		    (if (eq? flag 'set!)
 			(set! place (car rest))
 			(error (format #f "Unknown option passed to node: ~A -- ~A" flag place))))))))
-
+	 
 	 (set! nodes (map (lambda (node) ; compile pass
 			    (let ((values (cdr node)))
 			      (format #t "Creating node '~A'~%" (cadr (assoc 'sym-name values)))
@@ -105,14 +106,6 @@
 					this-node)))))
 			  nodes))
 
-;; 	 (map (lambda (node) (pretty-print (list ((node 'sym-name) 'get)
-;; 						 (list 'value ((node 'value) 'get-raw))
-;; 						 (map (lambda (slot) (list slot ((node slot) 'get)))
-;; 						      '(inputs outputs weights error-delta))))
-;; 		      (newline))
-;; 	      (map cadr nodes))
-
-
 	 (for-each (lambda (node)	; linker pass
 		     ((node 'inputs) 'set! (map (lambda (node) (cadr (assoc node nodes))) ((node 'inputs) 'get)))
 		     ((node 'outputs) 'set! (map (lambda (node) (cadr (assoc node nodes))) ((node 'outputs) 'get))))
@@ -124,7 +117,7 @@
 							'(inputs outputs weights error-delta value))))
 			(newline))
 		(map cadr nodes)))
-
+	 
 	 (define (back-prop node)
 	   ;; node => closure-object
 
@@ -132,7 +125,7 @@
 	     ;; This takes a list of nodes and a list of weights,
 	     ;; which are mapped to eachother linearly. This finds the
 	     ;; weight associated with `node' (see above scope)
-
+	     
 	     ;; OPTIMIZATION NOTE: Node-weight lookup could be
 	     ;; improved through some complex mechanism in the linking
 	     ;; pass
@@ -143,8 +136,8 @@
 		 (if (eq? (car search-nodes) node)
 		     (car search-weights)
 		     (searcher (cdr search-nodes) (cdr search-weights)))))
-		 
-
+	   
+	   
 	   (let ((bad-node (find-matching-item ((node 'outputs) 'get)
 					       (lambda (out-node)
 						 (not ((out-node 'error-delta) 'get))))))
@@ -166,60 +159,61 @@
 						       (+ this-weight (* learning-rate delta ((in-node 'value) 'get))))
 						     ((node 'weights) 'get)
 						     ((node 'inputs) 'get)))))
-						 
 		   (for-each back-prop ((node 'inputs) 'get))))))
 
-	 (case op
-	   ((run)			; inputs are in values
-	    (for-each (lambda (node) ((node 'value) 'set! #f)) (map cadr nodes)) ; clear memoized values
-	    (pretty-print-self)
-	    (format #t "Setting input nodes to: ~A~%" input-values)
-	    (for-each (lambda (node) ((node 'value) 'set! (pop! input-values))) (map (lambda (sym) (cadr (assoc sym nodes)))
-										     '(input-nodes ...)))
-	    (write-string "###########################################\n")
-	    (pretty-print-self)
-	    (write-string "###########################################\n")
-	    (map (lambda (sym) (((cadr (assoc sym nodes)) 'value) 'get)) '(output-nodes ...)))
-
-	   ((debug-nodes-pretty)
-	    (map (lambda (node) (pretty-print (list ((node 'sym-name) 'get)
-						    (map (lambda (slot) (list slot ((node slot) 'get-raw)))
-							 '(inputs outputs weights error-delta value))))
-			 (newline))
-		 (map cadr nodes)))
-
-	   ((debug-nodes)
-	    ;; For debugging purposes
-	    nodes)
-	      
-	   ((init)
-	    ;; Set node weights to random values
-	    (for-each (lambda (node)
-			((node 'weights) 'set!
-			 (map (lambda (nul) (random-real)) ((node 'weights) 'get))))
-		      (map cadr nodes)))
-
-	   ((last-errors)
-	    ;; Returns error deltas of output nodes
-	    (map (lambda (node) ((node 'error-delta) 'get)) (map (lambda (sym) (cadr (assoc sym nodes))) '(output-nodes ...))))
-
-	   ((train)
-	    ;; Run backprop algorithm on network
-	    ;; Inputs: (input1 input2 ...) (target1 target2 ...)
-	    (for-each (lambda (node) ((node 'error-delta) 'set! #f)) (map cadr nodes)) ; clear error deltas
-	    (let* ((inputs (car input-values))
-		   (targets (cadr input-values))
-		   (first-pass (apply net-name `(run ,@inputs)))
-		   (deltas (map (lambda (output target) (* (- target output) (- 1 output) output)) first-pass targets)))
-	      (for-each (lambda (node delta)
-			  (let ((self-node (cadr (assoc node nodes))))
-			    ((self-node 'error-delta) 'set! delta)
-			    ((self-node 'weights) 'set!
-			     (map (lambda (this-weight in-node)
-				    (+ this-weight (* learning-rate delta ((in-node 'value) 'get))))
-				  ((self-node 'weights) 'get)
-				  ((self-node 'inputs) 'get)))
-			    (for-each back-prop ((self-node 'inputs) 'get))))
-			'(output-nodes ...) deltas)))
-	   (else
-	    (error "bad operation on neural net"))))))))
+	 (set! net-name
+	     (lambda (op . input-values)
+	       (case op
+		 ((run)			; inputs are in values
+		  (for-each (lambda (node) ((node 'value) 'set! #f)) (map cadr nodes)) ; clear memoized values
+;; 		  (pretty-print-self)
+;; 		  (format #t "Setting input nodes to: ~A~%" input-values)
+		  (for-each (lambda (node) ((node 'value) 'set! (pop! input-values))) (map (lambda (sym) (cadr (assoc sym nodes)))
+											   '(input-nodes ...)))
+;; 		  (write-string "###########################################\n")
+;; 		  (pretty-print-self)
+;; 		  (write-string "###########################################\n")
+		  (map (lambda (sym) (((cadr (assoc sym nodes)) 'value) 'get)) '(output-nodes ...)))
+		 
+		 ((debug-nodes-pretty)
+		  (map (lambda (node) (pretty-print (list ((node 'sym-name) 'get)
+							  (map (lambda (slot) (list slot ((node slot) 'get-raw)))
+							       '(inputs outputs weights error-delta value))))
+			       (newline))
+		       (map cadr nodes)))
+		 
+		 ((debug-nodes)
+		  ;; For debugging purposes
+		  nodes)
+		 
+		 ((init)
+		  ;; Set node weights to random values
+		  (for-each (lambda (node)
+			      ((node 'weights) 'set!
+			       (map (lambda (nul) (random-real)) ((node 'weights) 'get))))
+			    (map cadr nodes)))
+		 
+		 ((last-errors)
+		  ;; Returns error deltas of output nodes
+		  (map (lambda (node) ((node 'error-delta) 'get)) (map (lambda (sym) (cadr (assoc sym nodes))) '(output-nodes ...))))
+		 
+		 ((train)
+		  ;; Run backprop algorithm on network
+		  ;; Inputs: (input1 input2 ...) (target1 target2 ...)
+		  (for-each (lambda (node) ((node 'error-delta) 'set! #f)) (map cadr nodes)) ; clear error deltas
+		  (let* ((inputs (car input-values))
+			 (targets (cadr input-values))
+			 (first-pass (apply net-name `(run ,@inputs)))
+			 (deltas (map (lambda (output target) (* (- target output) (- 1 output) output)) first-pass targets)))
+		    (for-each (lambda (node delta)
+				(let ((self-node (cadr (assoc node nodes))))
+				  ((self-node 'error-delta) 'set! delta)
+				  ((self-node 'weights) 'set!
+				   (map (lambda (this-weight in-node)
+					  (+ this-weight (* learning-rate delta ((in-node 'value) 'get))))
+					((self-node 'weights) 'get)
+					((self-node 'inputs) 'get)))
+				  (for-each back-prop ((self-node 'inputs) 'get))))
+			      '(output-nodes ...) deltas)))
+		 (else
+		  (error "bad operation on neural net"))))))))))
