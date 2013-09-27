825e4fd4462397099f105ba1efb7af73af5c60de
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2013-09-26 20:02:32 -0600

    - working on backprop algorithm with matricies

--- neural_network/matrix.scm
+++ neural_network/matrix.scm
@@ -43,18 +43,28 @@ Syntax description:
 
 	     ;; These are lists of 2D vectors
 	     (hidden-layers (map (lambda (layer) (list->vector (map list->vector layer))) '(hidden-layer-definition ...)))
-	     (hidden-errors (map (lambda (layer) (list->vector (map (lambda (node) (list->vector (map (lambda (nul) 0) node))) layer))) '(hidden-layer-definition ...)))
-	     (hidden-values (map (lambda (layer) (list->vector (map (lambda (node) (list->vector (map (lambda (nul) 0) node))) layer))) '(hidden-layer-definition ...)))
+	     (hidden-layers-r '())
+	     (hidden-errors (map (lambda (layer) (list->vector (map (lambda (node) (list->vector (map (lambda (nul) 0) node))) layer)))
+				 '(hidden-layer-definition ...)))
+	     (hidden-errors-r '())
+	     (hidden-values (map (lambda (layer) (list->vector (map (lambda (node) (list->vector (map (lambda (nul) 0) node))) layer)))
+				 '(hidden-layer-definition ...)))
+	     (hidden-values-r '())
 
 	     ;; This is a 2D vector
 	     (output-layer (list->vector (map list->vector 'output-definition)))
+	     (output-errors (list->vector (map (lambda (nul) 0) 'output-definition)))
 	     (output-values (list->vector (map (lambda (node) (list->vector (map (lambda (nul) 0) node))) 'output-definition))))
+
+	 (set! hidden-layers-r (reverse hidden-layers))
+	 (set! hidden-errors-r (reverse hidden-errors))
+	 (set! hidden-values-r (reverse hidden-values))
 
 	 (set! net-name
 	       (lambda (op . args)
 		 (define (matrix-* matrix vect)
 		   ;; Assumes multiplying n-m matrix by m-1 matrix, which I'll call a vector here
-		   (vector->map (lambda (node-row) ; NOTE: There's probably room for some optimization here (e.g. eliminate calls to vector->list)
+		   (vector-map (lambda (node-row) ; NOTE: There's probably room for some optimization here (e.g. eliminate calls to vector->list)
 				  (apply + (map * (vector->list node-row) (vector->list vect))))
 				matrix))
 
@@ -64,21 +74,53 @@ Syntax description:
 		    ((null? layers) (error "Must have some layers in the network before you can run it!"))
 		    ((null? input)  (error "No input given for neural network!"))
 		    (else
-		     (let ((result (vector->map activation (matrix-* (car layers) input))))
+		     (let ((result (vector-map activation (matrix-* (car layers) input))))
 		       (set-car! values result) ; will this work?
 		       (if (null? (cdr layers))
 			   result
 			   (feed-forward result (cdr layers) (cdr values)))))))
 
 		 (case op
+		   ;; Primary methods
 		   ((run)
+		    ;; Runs the neural network: (net 'run (list input1 input2 ...))
 		    (set! input-values (list->vector (car args)))
-		    (set! output-values (feed-forward input-values `(,@hidden-layers output-layer) `(,@hidden-values output-values)))
+		    (set! output-values (feed-forward input-values `(,@hidden-layers ,output-layer) `(,@hidden-values ,output-values)))
 		    output-values)
+		   
+		   ((train)
+		    ;; Runs the backprop algorithm: (net 'train (list input1 input2 ...) (list target1 target2 ...))
+		    (let ((inputs (list->vector (car args)))
+			  (targets (list->vector (cadr args))))
+		      (set! input-values inputs)
+		      (set! output-values (feed-forward input-values `(,@hidden-layers ,output-layer) `(,@hidden-values ,output-values)))
+		      (set! output-errors (vector-zip (lambda (output target) (* (- target output) (- 1 output) output)) output-values targets))
+		      (set! output-layer (vector-zip (lambda (node delta) (vector-zip (lambda (weight value)
+											(+ weight (* learning-rate delta value)))
+										      node
+										      
 
-		   ((debug-layers)
+		   ;; Debugging methods
+		   ((debug-hidden-layers)
 		    (pretty-print hidden-layers)
 		    (newline))
+
+		   ((debug-layers)
+		    (map (lambda (layer) (vector-map (lambda (node) (pretty-print node)) layer) (newline)) hidden-layers)
+		    (pretty-print output-layer))
 		   
 		   (else
 		    (error (format #f "Unknown option to neural net: '~A'~%" op)))))))))))
+
+(define (vector-zip func vec1 vec2)
+  (let ((end-val (apply min (map vector-length (list vec1 vec2)))))
+    (define (zipper i acc)
+      (if (>= (+ i 1) end-val)
+	  (list->vector (reverse! acc))
+	  (zipper (+ i 1) (cons (func (vector-ref vec1 i) (vector-ref vec2 i)) acc))))
+    (zipper 0 '())))
+
+(define (full-vector-set! vect obj)
+  ;; This should set the vector using vector-set!
+  (
+  
--- neural_network/t/matrix_text.scm
+++ neural_network/t/matrix_text.scm
@@ -15,9 +15,14 @@
   ((0.3 0.9))				; output nodes
   ((0.1 0.8) (0.4 0.6)))		; hidden layer
 
-(test-net00 'debug-layers)
+(let ((result1 (vector-ref (test-net00 'run '(0.35 0.9)) 0))
+      (result2 (vector-ref (test-net00 'run '(0.9 0.35)) 0)))
+  (ok (good result1 0.69 0.01) (format #f "test-net00 ran correctly: ~A" result1))
+  (ok (not (= result1 result2)) (format #f "test-net00 returned different result correctly: ~A" result2)))
 
-(let ((result1 (test-net00 'run '(0.35 0.9))))
-  (pretty-print result1)
-  (newline)
-  (format #t "Should be: ~A~%" 0.69))
+(let ((result1 (vector-ref (test-net00 'run 0.35 0.9) 0))
+      (result2 0))
+  (ok (good result1 0.69 0.01) (format #f "test-net00 ran correctly (again): ~A" result1))
+  (test-net00 'train '(0.35 0.9) '(0.5))
+  (set! result2 (vector-ref (test-net00 'run 0.35 0.9) 0))
+  (ok (good (abs (- result2 0.5)) 0.18205 0.001) (format #f "test-net00 trained correctly: ~A" result2)))
