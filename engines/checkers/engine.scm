abdbfd8798220fddbc1a76e20086a3d8a604302a
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2013-10-22 19:14:25 -0600

    - working on board printing

--- engines/checkers/engine.scm
+++ engines/checkers/engine.scm
@@ -4,6 +4,12 @@
 
 (load-option 'format)
 
+;; Global variables
+(define *white-pawn* "w")
+(define *white-king* "W")
+(define *black-pawn* "b")
+(define *black-king* "B")
+
 ;; Macros
 (define-syntax dotimes
   (syntax-rules ()
@@ -16,10 +22,24 @@
   (syntax-rules ()
     ((_ var) (begin (set! var (+ var 1)) var))))
 
+(define-syntax push!
+  (syntax-rules ()
+    ((push! datum place)
+     (set! place (cons datum place)))))
+
+(define-syntax pop!
+  (syntax-rules ()
+    ((pop! place)
+     (let ((temp1 (car place)))
+       (set! place (cdr place))
+       temp1))))
+
 
 ;; Constructer function
 
 (define (make-board)
+  ;; Returns a new checkers board
+
   ; Internal macros
   (define-syntax getter-setter
     (syntax-rules ()
@@ -29,24 +49,25 @@
 	       ((eq? op 'get) var)
 	       (else (error (format #f "Bad option to getter-setter: ~A" 'var))))))))
 
+  ;; Board description:
+  ;;   8x4 game board
+  ;;   Sides: 2, white and black
+  ;;   Squares:
+  ;;     0  => empty
+  ;;     1  => white pawn
+  ;;     2  => white king
+  ;;     -1 => black pawn
+  ;;     -2 => black king
+
   ; Private values
-  (let ((white #((1 1 1 1)
-		 (1 1 1 1)
-		 (1 1 1 1)
-		 (0 0 0 0)
-		 (0 0 0 0)
-		 (0 0 0 0)
-		 (0 0 0 0)
-		 (0 0 0 0)))
-		   
-	(black #((0 0 0 0)
-		 (0 0 0 0)
-		 (0 0 0 0)
-		 (0 0 0 0)
-		 (0 0 0 0)
-		 (1 1 1 1)
-		 (1 1 1 1)
-		 (1 1 1 1)))
+  (let ((board #(#(1 1 1 1)
+		 #(1 1 1 1)
+		 #(1 1 1 1)
+		 #(0 0 0 0)
+		 #(0 0 0 0)
+		 #(-1 -1 -1 -1)
+		 #(-1 -1 -1 -1)
+		 #(-1 -1 -1 -1)))
 	(turn 'white))
 
     (lambda (op . args)
@@ -57,12 +78,19 @@
 
 
 ;; Board utilities
-(define (format-board whites blacks)
-  (string-append
-   (format #f "
+(define (format-board board)
+  (reduce string-append
+	  (format #f "
      1   2   3   4   5   6   7   8
-   +---+---+---+---+---+---+---+---+")
-   ;; NOT FINISHED HERE))
+   +---+---+---+---+---+---+---+---+~%")
+	  (vector-mapn (lambda (row idx)
+			 (format #f (if (even? idx)
+					" ~A | ~A |   | ~A |   | ~A |   | ~A |   |~%"
+					" ~A |   | ~A |   | ~A |   | ~A |   | ~A |~%")
+				 ;;;; STILL WORKING HERE
+
+		board #(1 2 3 4 5 6 7 8))))
+
 
 ;; Helper functions
 
@@ -72,3 +100,39 @@
       (list 0 num)
       (list (truncate (/ num 10))
 	    (remainder num 10))))
+
+(define (vector-reduce func vec)
+  (let ((leng (vector-length vec)))
+    (define (loop counter acc)
+      (if (= (+ counter 1) leng)
+	  acc
+	  (loop (+ counter 1) (func acc (vector-ref vec (+ counter 1))))))
+    (loop 0 (vector-ref vec 0))))
+
+(define (vector-zip func vec1 vec2)
+  (let ((end-val (apply min (map vector-length (list vec1 vec2)))))
+    (define (zipper i acc)
+      (if (> (+ i 1) end-val)
+	  (list->vector (reverse! acc))
+	  (zipper (+ i 1) (cons (func (vector-ref vec1 i) (vector-ref vec2 i)) acc))))
+    (zipper 0 '())))
+
+(define (vector-mapn func . vects)
+  (let* ((end-val (apply min (map vector-length vects)))
+	 (acc (make-vector end-val)))
+    (do ((i 0 (+ i 1)))
+	((>= i end-val) acc)
+      (vector-set! acc i (apply func (map (lambda (vec) (vector-ref vec i)) vects))))))
+
+(define (vector-clobber! target vals)
+  (let ((leng (vector-length target)))
+    (do ((i 0 (+ i 1)))
+	((= i leng) target)
+      (vector-set! target i (vector-ref vals i)))))
+
+(define (range lower upper)
+  (define (loop i acc)
+    (if (< i lower)
+	(reverse! acc)
+	(loop (- i 1) (cons i acc))))
+  (loop upper '()))
