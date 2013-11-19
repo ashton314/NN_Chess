44a728c626576fb9ec4cb53f8d57852f5db861af
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2013-11-18 21:50:10 -0700

    - starting work on neural network; I've just got a few of the files in   place - WARNING: Read project_log.txt before continuing work

---
+++ engines/checkers/brain.scm
@@ -0,0 +1,7 @@
+;;; AI Routines
+;;; Ashton Wiersdorf
+;;; Part of the NN_Chess project
+
+;; Load neural network
+(open-input-file "NN_DATA/network.scm")	; do something with this, then close it
+;;; WARNING!! NN_DATA/network.scm is not finished yet!!
