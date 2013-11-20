697fda2106ca36d29bbd9c932ee15da59918c057
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2013-11-20 08:22:06 -0700

    - working on feature detection

--- engines/checkers/brain.scm
+++ engines/checkers/brain.scm
@@ -3,5 +3,60 @@
 ;;; Part of the NN_Chess project
 
 ;; Load neural network
-(open-input-file "NN_DATA/network.scm")	; do something with this, then close it
-;;; WARNING!! NN_DATA/network.scm is not finished yet!!
+(define *network-stream* (open-input-file "NN_DATA/network.scm"))
+(define *output-nodes* (read *network-stream*))
+(define *hidden-layer* (read *network-stream*))
+(close-port *network-stream*)
+
+  ; etc.
+
+
+;; Feature detectors
+;; NOTE: I'll need to scale these to values between -1 and 1 for the network
+(define (pawn-compare board)
+  ;; Returns #white_pawns - #black_pawns
+
+  ;; NOTE: I'm sure there's room for optimizing this routine and
+  ;; king-compare by memotizing the boards
+  (let ((whites 0)
+	(blacks 0))
+    (vector-map (lambda (row)
+		  (vector-map (lambda (cell) (cond ((= cell 1) (inc! whites)) ((= cell -1) (inc! blacks)))) row)) board)
+    (- whites blacks)))
+
+(define (king-compare board)
+  ;; Returns #white_kings - #black_kings
+
+  (let ((whites 0)
+	(blacks 0))
+    (vector-map (lambda (row)
+		  (vector-map (lambda (cell) (cond ((= cell 2) (inc! whites)) ((= cell -2) (inc! blacks)))) row)) board)
+    (- whites blacks)))
+
+(define (advancement-potential board side)
+  ;; Returns how many pieces are within two spaces of the relitive
+  ;; "back" of the board
+  (let ((count 0)
+	(val (if (eq? side 'white) 1 (if (eq? side 'black) -1 (error "Unknown side passed to advancement-potential")))))
+    (vector-map (lambda (row)
+		  (vector-map (lambda (cell) (if (= cell val) (inc! count))) row))
+		(if (eq? side 'white)
+		    (vector-tail board 5)
+		    (vector-head board 3)))
+    count))
+
+(define (white-advancement-potential board)
+  (advancement-potential board 'white))
+
+(define (black-advancement-potential board)
+  (advancement-potential board 'black))
+
+(define (analize-movement board)
+  (let* ((white-moves (possible-moves board 'white))
+	 (black-moves (possible-moves board 'black))
+
+	 (count-white-moves (length white-moves))
+	 (count-black-moves (length black-moves)))
+    ;; NOT FINISHED HERE
+
+    ))
