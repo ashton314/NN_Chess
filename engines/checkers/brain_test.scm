1528e8c3e8404d5199dd6dbbc3e8c06e973241e3
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2013-11-22 14:57:05 -0700

    - all feature detection tests passing

--- engines/checkers/brain_test.scm
+++ engines/checkers/brain_test.scm
@@ -15,7 +15,30 @@
       (is (car results) 6 "white has 6 moves in board0")
       (is (cadr results) 15 "black has 15 moves in board0")
       (is (caddr results) 0 "white has no endangered pieces")
-      (is (cadddr results) 1 "black has one endangered piece"))))
+      (is (cadddr results) 1 "black has one endangered piece"))
+
+    (set! board #(#(0 0 0 0) #(1 0 0 1) #(-1 0 -2 1) #(1 0 -1 0) #(1 0 -1 -1) #(-1 -1 0 0) #(-1 -1 2 0) #(-1 -1 0 0)))
+    (is (pawn-compare board) -5 "pawn comparison works for board1")
+    (is (king-compare board) 0 "king comparison works for board1")
+    (is (white-advancement-potential board) 0 "white advancement potential works for board1")
+    (is (black-advancement-potential board) 1 "black advancement potential works for board1")
+    (let ((results (analyze-movement board)))
+      (is (car results) 6 "white has 6 moves in board1")
+      (is (cadr results) 10 "black has 10 moves in board1")
+      (is (caddr results) 2 "white has 2 endangered pieces")
+      (is (cadddr results) 1 "black has one endangered piece"))
+
+    (set! board #(#(0 0 0 0) #(1 0 -2 1) #(0 0 1 1) #(1 1 -1 1) #(1 -1 0 -1) #(-1 -1 0 -1) #(-1 -1 -1 0) #(-1 -1 0 0)))
+    (is (pawn-compare board) -3 "pawn comparison works for board2")
+    (is (king-compare board) -1 "king comparison works for board2")
+    (is (white-advancement-potential board) 0 "white advancement potential works for board2")
+    (is (black-advancement-potential board) 0 "black advancement potential works for board2")
+    (let ((results (analyze-movement board)))
+      (is (car results) 4 "white has 4 moves in board2")
+      (is (cadr results) 8 "black has 8 moves in board2")
+      (is (caddr results) 1 "white has one endangered piece")
+      (is (cadddr results) 2 "black has two endangered pieces"))))
+
 
 ;; High-level chunks
 (feature-recognition)
