375954e3b97f12e1ff019293170d147c1e9673ba
Ashton Wiersdorf (Sirius) <ashton.wiersdorf@mailblock.net>
2014-02-06 21:20:54 -0700

    - working on debugging neural network creation routines

--- src/neural_networks/neural_network.scm
+++ src/neural_networks/neural_network.scm
@@ -20,11 +20,11 @@
        (set! place (cdr place))
        temp1))))
 
-(define (sigmoid val)
+(define-integrable (sigmoid val)
   ;; Sigmoid function
   (/ 1 (+ 1 (exp (- val)))))
 
-(define (sigmoid-derivitive val)
+(define-integrable (sigmoid-derivitive val)
   ;; d/dx sigmoid
   (* val (- 1 val)))
 
@@ -47,13 +47,20 @@ Usage:
 	(case op
 	  ;; primary routines
 	  
+	  ;; setters
+	  ((set-learning-rate) (set! learning-rate (car args)))
+	  ((set-layers) (set! layers (car args)))
+
+	  ;; getters
+	  ((get-learning-rate) learning-rate)
+	  ((get-layers) layers)
+
 	  ;; debugging routines
-	  ((layers) layers)
 	  ((pp-layers) (map pp layers)))))))
 
 
 ;; utility routines
-(define (initialize-layers input-count all-layers)
+(define-integrable (initialize-layers input-count all-layers)
   (define (loop previous-input-count layers acc)
     (if (null? layers)
 	(reverse! acc)
@@ -70,7 +77,7 @@ Usage:
 	       (loop (length (car layers)) (cdr layers)
 		     (cons (map
 			    (lambda (node)
-			      (if (not (= (length node) (+ 1 previous-inputs)))
+			      (if (not (= (length node) (+ 1 previous-inputs))) ;; bug here (and maybe vector-utils::randomize)
 				  (error (format #f "too few weights specified in layer ~A (you might have forgotten to account for the bias)" node))
 				  (if (for-all? node (lambda (n) (and (integer? n) (= n 0))))
 				      (randomize node)                            ; cons->vector mods here
